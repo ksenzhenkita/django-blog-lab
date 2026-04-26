391c2680f07c090007798ccb90ed29f205cd3074
ksenzhenkita <tetiana.ksenzhenko@gmail.com>
2026-04-26 15:49:38 +0200

    Fix B105: Moved SECRET_KEY to .env file for security

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+# Завантажуємо змінні з файлу .env
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j_2wmib$w*)re4jo)hjb^ix6!^rr#e!0i33mf)8i&ir=d=-1%4' # nosec
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
